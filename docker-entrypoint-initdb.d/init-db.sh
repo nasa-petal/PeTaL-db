9b5be1e0c17b55fa176585fa0d09667ffc88ab2a
Brandon Ruffridge <bruffridge@nasa.gov>
2020-10-05 16:29:21 -0400

    updated user

--- docker-entrypoint-initdb.d/init-db.sh
+++ docker-entrypoint-initdb.d/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER petal_admin;
+    CREATE USER petal_admin WITH PASSWORD 'mysecretpassword';
     CREATE DATABASE petal;
     GRANT ALL PRIVILEGES ON DATABASE petal TO petal_admin;
 EOSQL
