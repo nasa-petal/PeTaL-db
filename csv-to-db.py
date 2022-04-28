1f3a0391f0a711d2b893b2a55b28e8498c0fa9b3
dsmith111 <davidcsmith12@gmail.com>
2022-04-28 17:47:22 -0400

    Species-label mapping and vice versa

--- csv-to-db.py
+++ csv-to-db.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import boto3
 import re
+import json
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -10,16 +11,21 @@ filename = './biomim_pred_species.csv'
 filename_labels = './labels.csv'
 # need to update this to include rows for common to scientific mappings and rows for just the common names
 filename_creatures = './creaturesdb.csv'
+filename_species_label = "./species-label.json"
+filename_label_species = "./label-species.json"
 
 def main():
     csvfile = open(filename)
     csvfile_labels = open(filename_labels)
     csvfile_species = open(filename_creatures)
+    csvfile_species_mapping = json.load(open(filename_species_label))
+    csvfile_label_mapping = json.load(open(filename_label_species))
     
     write_species_to_dynamo(csv.DictReader(csvfile_species))
     write_labels_to_dynamo(csv.DictReader(csvfile_labels))
     write_to_dynamo(csv.DictReader(csvfile))
-
+    write_species_map_to_dynamo(csvfile_species_mapping)
+    write_species_map_to_dynamo(csvfile_label_mapping)
     return print("Done")
 
 def slicedict(d, s):
@@ -37,6 +43,34 @@ def write_labels_to_dynamo(rows):
                     'level3': row['Level3']
                 }
             )
+def write_species_map_to_dynamo(keyVal):
+    table = dynamodb.Table(tableName)
+    with table.batch_writer() as batch:
+        for species in keyVal:
+            for label in keyVal[species]:
+                baseLabel = re.sub("LABEL-","",label)
+                batch.put_item(
+                    Item={
+                        'PartitionKey': "SPECIES-"+species,
+                        'SortKey': label,
+                        'level3': [baseLabel]
+                    }
+                )
+
+# Write label to species function mapping to dynamo
+def write_label_map_to_dynamo(keyVal):
+    table = dynamodb.Table(tableName)
+    with table.batch_writer() as batch:
+        for label in keyVal:
+            for species in keyVal[label]:
+                batch.put_item(
+                    Item={
+                        'PartitionKey': label,
+                        'SortKey': "SPECIES-"+species,
+                        'speciesName': species
+                    }
+                )
+
 def write_species_to_dynamo(rows):
     table = dynamodb.Table(tableName)
     with table.batch_writer() as batch:
